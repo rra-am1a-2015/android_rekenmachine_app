87119a01e856700e795746e8d22b73d2b5d83e1b
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-03 10:47:03 +0100

    Rekenmachine app

--- app/src/main/java/com/adruijter/lesson2_lineair_layout/MainActivity.java
+++ app/src/main/java/com/adruijter/lesson2_lineair_layout/MainActivity.java
@@ -9,18 +9,44 @@ public class MainActivity extends AppCompatActivity
 {
     // Fields
     private int number1;
+    private int number2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 
     public void decrementNumber1(View view)
     {
         number1 = number1 - 1;
         TextView txtNumber = (TextView) findViewById(R.id.txtNumber1);
+        TextView txttest = (TextView) findViewById(R.id.number3);
         txtNumber.setText(Integer.toString(number1));
+        txttest.setText(Integer.toString(number1));
+    }
+
+    public void decrementNumber2(View view)
+    {
+        number2 = number2 - 1;
+        TextView txtNumber = (TextView) findViewById(R.id.txtNumber2);
+        txtNumber.setText(Integer.toString(number2));
+    }
+
+    public void incrementNumber1(View view)
+    {
+        number1 = number1 + 1;
+        TextView txtNumber = (TextView) findViewById(R.id.txtNumber1);
+        txtNumber.setText(Integer.toString(number1));
+    }
+
+    public void incrementNumber2(View view)
+    {
+        number2 = number2 + 1;
+        TextView txtNumber = (TextView) findViewById(R.id.txtNumber2);
+        txtNumber.setText(Integer.toString(number2));
     }
 }
